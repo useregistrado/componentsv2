b4c1e42744860216af5ea1f455a252ff9e1693c6
useregistrado <oscar.vargas07@uptc.edu.co>
2021-09-27 23:11:27 -0500

    un commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="navbar">
+      <div class="navbar_l">
+        <img src="../assets/logo.png" alt="">
+        <h2>T&T Tecnologias</h2>
+      </div>
+      <div class="navbar_r">
+        <div class="btn-pay-div"><a href="" class="btn-pay">PAGAR</a></div>
+        <div><a href="#">Servicios</a></div>
+        <div><a href="#">Nosotros</a></div>
+        <div><a href="#">Contacto</a></div>
+        <div class="separator"></div>
+        <div><a href="#">Iniciar Sesión</a></div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
---
+++ src/components/slider.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <b-img class="img-slider" src="main.jpg" fluid alt="Responsive image"></b-img>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'slider'
+}
+</script>
