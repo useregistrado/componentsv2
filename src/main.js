72bfa083b48d5dadf6e8bc803e90ed52984bccb2
useregistrado <oscar.vargas07@uptc.edu.co>
2021-09-27 23:09:59 -0500

    version inicial

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
