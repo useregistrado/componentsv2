cae674f6d2e48d3e28d6930cb4ecbd69d601cab6
useregistrado <oscar.vargas07@uptc.edu.co>
2021-10-06 03:13:15 -0500

    sitio final vista de computador

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <Navbar msg="Welcome to Your Vue.js App"/>
     <slider/>
     <services/>
+    <contact/>
+    <mision/>
+    <footer2/>
+
   </div>
 </template>
 
@@ -10,13 +14,19 @@
 import Navbar from './components/Navbar.vue'
 import slider from './components/slider.vue'
 import services from './components/services.vue'
+import contact from './components/contact.vue'
+import mision from './components/mision.vue'
+import footer2 from './components/footer.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
     slider,
-    services
+    services,
+    contact,
+    mision,
+    footer2
   }
 }
 </script>
---
+++ src/components/footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="footer">
+        <div class="footer_l">
+            <img src="../assets/logo.png" alt="">
+            <h2>T&T Tecnologias</h2>
+        </div>
+      <div class="footer_r">
+        <div class="btn-opt"> <img src="../assets/call.png" alt=""> <a href="#">+57 3023257043</a></div>
+        <div class="btn-opt"> <img src="../assets/mail.png" alt=""><a href="#">contacto@tyt-tecnologias.com.co</a></div>
+        <div class="btn-opt"> <img src="../assets/location.png" alt=""><a href="#">Cra. 17 # 15 - 74 - Aguazul, Casanare</a></div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'footer2'
+}
+</script>
